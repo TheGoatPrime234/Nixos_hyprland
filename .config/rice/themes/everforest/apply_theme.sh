1bda76ec6d78560319fce8c8a9e5659c18685349
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-03-31 21:42:37 +0200

    new try

---
+++ .config/rice/themes/everforest/apply_theme.sh
@@ -0,0 +1,3 @@
+cp waybar/current.css ~/.config/waybar/current.css
+cp swaync/current.css ~/.config/swaync/current.css
+cp rofi/current.rasi ~/.config/rofi/current.rasi
---
+++ .config/rofi/launcher.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+dir="$HOME/.config/rofi/launchers/"
+theme='style-2'
+## Run
+rofi \
+    -show drun \
+    -theme main.rasi
