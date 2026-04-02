075713db4f554222c96d5e92f30010d9b424a3fc
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-03 01:18:56 +0200

    Neuer Versuch Brave zum laufen zu bekommen

--- nixos-config/apps.nix
+++ nixos-config/apps.nix
@@ -26,11 +26,12 @@
         wrapProgram $out/bin/brave \
           --add-flags "--password-store=basic" \
           --add-flags "--disable-gpu-sandbox" \
-          --add-flags "--use-gl=egl" \
+          --add-flags "--use-gl=desktop" \
           --add-flags "--enable-features=UseOzonePlatform" \
           --add-flags "--ozone-platform=wayland"
       '';
     })
+
     # Hyprland Setup
     hyprland
     hyprpaper
