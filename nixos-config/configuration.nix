0d6b0f63679ba56fcf055e76a07e122b1132bcde
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-02 22:41:00 +0200

    Tweaks

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -72,7 +72,6 @@
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };  
-
   # Hyprland
   programs.hyprland = {
     enable = true;
--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -15,6 +15,7 @@
           # Hier laden wir deine alten Dateien in die Flake
           ./hardware-configuration.nix
           ./configuration.nix
+	  ./fonts.nix
 	  ./apps.nix
         ];
       };
