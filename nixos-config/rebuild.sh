42ef058be463f3ed837c02b1d6ea1122eff55a49
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-06 20:48:50 +0200

    More Git add

--- nixos-config/rebuild.sh
+++ nixos-config/rebuild.sh
@@ -9,6 +9,22 @@ HOST="desktop"
 cp /etc/nixos/hardware-configuration.nix "hardware-configuration-${HOST}.nix"
 
 git add . 
+cd ~/.config
+git add btop/
+git add cava/
+git add hypr/
+git add kitty/
+git add nvim/
+git add quickshell/
+git add quickshell/
+git add spicetify/
+git add swaync/
+git add syncthing/
+git add waybar/
+git add wlogout/
+git add yazi/
+git add rofi/
+cd ~/nixos-config
 echo "Starte Rebuild für Host: ${HOST}..."
 sudo nixos-rebuild switch --flake .#${HOST} -v
 
